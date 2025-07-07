44a1524306ca1068026236a74e69af9ce41e296d
alaa-assas <alaa.assas1997@gmail.com>
2025-07-07 16:58:22 +0300

    fix error in social btn

--- src/components/SocailBtn/SocailBtn.tsx
+++ src/components/SocailBtn/SocailBtn.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { tabs } from "../../data/Social";
 import { useNavigate } from "react-router-dom";
 
 const SocailBtn: React.FC = () => {
     const navigate = useNavigate();
+    const [activeTab, setActiveTab] = useState<number | null>(null);
 
-  const handleClick = (action: "link" | "tel" | "mailto", target: string) => {
+  useEffect(() => {
+    if (activeTab !== null) {
+      const timer = setTimeout(() => setActiveTab(null), 3000);    
+      return () => clearTimeout(timer);
+    }
+  }, [activeTab]);
+
+  const handleClick = (index: number,action: "link" | "tel" | "mailto", target: string) => {
+    if (activeTab === index) {
     if (action === "tel") {
       window.location.href = `tel:${target}`;
     } else if (action === "link") {
       navigate(target)
     }else{
         window.location.href = `mailto:${target}`;
+    }}else{
+        setActiveTab(index);
     }
   };
 
@@ -20,9 +31,9 @@ const SocailBtn: React.FC = () => {
       {tabs.map((tab, index) => (
         <div
           key={index}
-          onClick={() => handleClick(tab.action, tab.target)}
-          className={`group flex items-center pl-2 pr-2 py-1 w-[40px] hover:w-[120px]
-          h-10 rounded-r-lg overflow-hidden cursor-pointer shadow-md ${tab.color}`}
+          onClick={() => handleClick(index, tab.action, tab.target)}
+          className={`group flex items-center overflow-hidden cursor-pointer shadow-md rounded-r-lg transition-all duration-300
+          pl-1 pr-2 py-1 h-10 ${activeTab === index ? "w-[100px]" : "w-[33px]"} ${tab.color}`}
         >
           <div className="flex items-center justify-center w-6 h-6 shrink-0">
             {tab.icon}
@@ -30,7 +41,7 @@ const SocailBtn: React.FC = () => {
           <span
             className={`
               ml-2 text-xs font-medium text-white whitespace-nowrap
-              opacity-0 group-hover:opacity-100
+              ${activeTab === index ? "opacity-100" : "opacity-0"}
               transition-opacity duration-300
             `}
           >
